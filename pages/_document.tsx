b94a6b2bcd31dbf51105f6122a7b32c0af63bce2
Daniel <daniel.bueno@talentomobile.com>
2023-04-08 18:09:55 +0200

    change script for Script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -19,12 +20,12 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
           crossOrigin="anonymous"
         />
-        <script
+        <Script
           src="https://kit.fontawesome.com/a076d05399.js"
           crossOrigin="anonymous"
         />
