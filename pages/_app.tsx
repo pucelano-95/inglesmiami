0d1fe9d7fc6626d6d99bd05494e0af1f28a32abd
Daniel <daniel.bueno@talentomobile.com>
2023-04-09 11:19:15 +0200

    fix integration with Script instead of script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Script from "next/script";
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossOrigin="anonymous"
+    />
+    <Script
+      src="https://kit.fontawesome.com/a076d05399.js"
+      crossOrigin="anonymous"
+    />
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
