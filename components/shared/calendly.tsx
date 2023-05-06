a8187b5eca6713e64c7375c7403da31c63fad8d5
Daniel <daniel.bueno@talentomobile.com>
2023-05-06 18:02:34 +0200

    adapt calendly widget

---
+++ components/shared/calendly.tsx
@@ -0,0 +1,10 @@
+export default function Calendly() {
+  return (
+    <div className="calendly-div">
+      <iframe
+        src="https://calendly.com/ralph-barrero-inglesmiami/clases?hide_gdpr_banner=1"
+        className="calendly-inline-widget"
+      ></iframe>
+    </div>
+  );
+}
