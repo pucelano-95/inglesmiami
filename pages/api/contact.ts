0959f24f1ea41939fedcabe8e5a594acff5f9d81
Daniel <daniel.bueno@talentomobile.com>
2023-05-07 12:42:35 +0200

    oauth for email

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -6,13 +6,28 @@ export default async function sendEmail(
   res: NextApiResponse
 ) {
   const nodemailer = require("nodemailer");
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  const transport =
+    process.env.NODE_ENV == "development"
+      ? {
+          service: "gmail",
+          auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD,
+          },
+        }
+      : {
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+            type: "OAuth2",
+            user: process.env.EMAIL_USERNAME,
+            clientId: process.env.EMAIL_CLIENT_ID,
+            clientSecret: process.env.EMAIL_CLIENT_SECRET,
+            refreshToken: process.env.EMAIL_CLIENT_REFRESH_TOKEN,
+          },
+        };
+  const transporter = nodemailer.createTransport(transport);
 
   const mailData = {
     from: req.body.email,
@@ -23,14 +38,15 @@ export default async function sendEmail(
 
   transporter.sendMail(mailData, function (err: any, info: any) {
     if (err) {
-      console.error(err);
       res.status(500).send({
         message: err,
       });
+      res.end();
       return;
     }
     res.send({
       data: API_MESSAGE_SUCCESS,
     });
+    res.end();
   });
 }
