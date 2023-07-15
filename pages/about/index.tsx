f8d7a6231d005ffffe934f04c58209fcd268acd6
Daniel <daniel.bueno@talentomobile.com>
2023-07-15 11:31:05 +0200

    upload changes in text

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,21 +5,33 @@ export default function About() {
     <Layout className="p-5 mx-auto text-center">
       <h1 className="display-4 fw-normal delauney-font">Acerca de nosotros</h1>
       <p className="fs-5 text-dark">
-        Bienvenidos a{" "}
-        <span className="transform-title">{process.env.NEXT_PUBLIC_TITLE}</span>
-        , somos una compañía dedicada a enseñar inglés a personas de habla
-        hispana de América Latina en Miami. Llevamos más de 30 años de
-        experiencia apoyando a individuos, migrantes y empresas, lograr sus
-        metas profesionales, académicas y personales, atreves del idioma. En
-        estos 30 años en Norte América, Latino América y Asia, hemos aprendido
-        que el aprendizaje del idioma debe ser CONVERSACIONAL y accesible para
-        toda persona que quiera superarse. Nuestra misión es empoderar a
-        nuestros alumnos a desarrollar su potencial máxima, convirtiéndonos en
-        líder mundial en servicios del idioma. Nuestro método es 100%
-        conversacional y el contenido de las sesiones se adapta, y crece, según
-        tanto sus necesidades, como a los cambios sociales. Nuestras lecciones
-        son una mezcla perfecta de profesionalismo con diversión, utilizando
-        grupos pequeños, que se adaptan a cualquier presupuesto.
+        ¡Bienvenidos a nuestra empresa de servicios lingüísticos en línea!
+        Nuestra misión es ayudarles a comunicarse de manera efectiva en un mundo
+        globalizado, proporcionando servicios lingüísticos de alta calidad tanto
+        a particulares como a empresas. Nuestro equipo está compuesto por
+        expertos lingüísticos experimentados que tienen pasión por ayudar a
+        nuestros clientes a alcanzar sus objetivos de comunicación. Ofrecemos
+        una amplia gama de servicios, incluyendo traducción profesional,
+        interpretación, formación lingüística y servicios de localización.
+        Trabajamos con una variedad de industrias, incluyendo la salud, el
+        derecho, las finanzas, la tecnología y más. En nuestra empresa,
+        entendemos la importancia de una comunicación clara y efectiva,
+        especialmente en el mundo interconectado de hoy en día. Es por eso que
+        nos enorgullece nuestro compromiso de ofrecer servicios lingüísticos
+        precisos y culturalmente apropiados a nuestros clientes. Nuestro enfoque
+        se centra en un servicio personalizado y atención al detalle. Trabajamos
+        en estrecha colaboración con cada cliente para entender sus necesidades
+        únicas y desarrollar soluciones lingüísticas personalizadas que
+        satisfagan esas necesidades. Utilizamos la última tecnología y
+        herramientas para garantizar una entrega eficiente y precisa de nuestros
+        servicios. Ya sea que necesiten servicios de traducción para un
+        documento legal, servicios de interpretación para una reunión de
+        negocios o formación lingüística para sus empleados, tenemos la
+        experiencia y los recursos para ayudarles a tener éxito. Nuestro equipo
+        está dedicado a ofrecer un servicio y soporte excepcionales en todo el
+        proceso. Gracias por considerar nuestra empresa para sus necesidades de
+        servicios lingüísticos. Esperamos trabajar con ustedes y ayudarles a
+        comunicarse de manera efectiva en un mundo globalizado.
       </p>
     </Layout>
   );
--- pages/services/test.tsx
+++ pages/services/test.tsx
@@ -1,6 +1,6 @@
 import Layout from "@/components/layout/layout";
 import Image from "next/image";
-import Calendly from "@/components/shared/calendly";
+import Link from "next/link";
 
 export default function Test() {
   return (
@@ -17,9 +17,19 @@ export default function Test() {
         style={{ width: "40%", height: "auto" }}
       />
       <p className="my-4 fs-5 text-dark">
-        ¡Puedes reservar una clase con nosotros aqui mismo!
+        ¿Estás buscando demostrar tu habilidad para hablar inglés a posibles
+        empleadores, universidades o agencias gubernamentales? ¡Nuestro examen
+        de nivel de inglés es una excelente manera de hacerlo! Puedes mostrar
+        tus habilidades lingüísticas en lectura, escritura, comprensión auditiva
+        y expresión oral con nuestro examen. Además, nuestro examen es
+        reconocido en todo el mundo, por lo que puedes tomarlo desde cualquier
+        lugar del mundo. No dejes que las dificultades del idioma te detengan.
+        ¡Toma nuestro examen de nivel de inglés hoy y abre numerosas
+        oportunidades!
       </p>
-      <Calendly />
+      <Link className="w-100 btn btn-lg btn-outline-primary" href="/booking">
+        Reserva una clase con nosotros
+      </Link>
     </Layout>
   );
 }
